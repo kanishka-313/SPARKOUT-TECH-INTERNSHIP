50cd5c1a9fe6a6306859ed2e11573ff2338d3d34
KANISHKA <kanishka313n@gmail.com>
2026-06-15 11:27:33 +0530

    Add files via upload

---
+++ Student marks.py
@@ -0,0 +1,13 @@
+A=int(input("Enter Your Mark 0 to 100:"))
+if(100>=A>=90):
+    print("GRADE A !!")
+elif(89>=A>=80):
+    print("GRADE B !!")
+elif(79>=A>=70):
+    print("GRADE B1 !!")
+elif(69>=A>=60):
+    print("GRADE C !!")
+elif(59>=A>=40):
+    print("GRADE C1 !!")
+elif(40>A):
+    print("FAIL !!")

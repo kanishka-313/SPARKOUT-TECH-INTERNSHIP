f5259aa960ecdaa7af1696f73f7fa431f0a06aee
KANISHKA <kanishka313n@gmail.com>
2026-06-15 11:27:04 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+a=int(input("Enter a Number A :"))
+b=int(input("Enter another Number:"))
+c=input("Enter any operations +,-,/,*,%:")
+if(c=='+'):
+    print(a+b)
+elif(c=='-'):
+    print(a-b)
+elif(c=='/'):
+    print(a/b)
+elif(c=='*'):
+    print(a*b)
+elif(c=='%'):
+    print(a%b)
+else:
+    print("Invalid Operation")
+
+
+            
